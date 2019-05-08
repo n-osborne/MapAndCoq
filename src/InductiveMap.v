cac5e4fbeeaf4ad78fd2dafd4ba8ac0534000851
Nicolas Osborne <nicolas.osborne@etudiant.univ-lille1.fr>
2019-05-08 21:37:20 +0200

    [implem] Define Inductive mapping and some functions

--- src/InductiveMap.v
+++ src/InductiveMap.v
@@ -9,7 +9,9 @@ Class EqDec (A:Type) :=
   {
     eqb : A -> A -> bool;
     eqb_leibniz : forall x y, eqb x y <-> x = y
-    }.
+  }.
+
+Notation "x =? y" := (eqb x y) (at level 60).
 
 Section DecidableKeys.
   (**
@@ -18,6 +20,74 @@ Section DecidableKeys.
    *)
   Generalizable Variable K.
   Context `{KDec: EqDec K}.
+
+  (**
+     A [mapping] is define as an inductive Type.
+     There is a typeclass constraint on K.
+   *)
+  Inductive mapping `{EqDec K} {E: Type} : Type :=
+  | empty : mapping
+  | record : K -> E -> mapping -> mapping.
+
+  (**
+     The [find] function return an [option] of the type of the elements.
+   *)
+  Fixpoint find {E:Type} (k1:K) (m:mapping): option E :=
+    match m with
+    | empty => None
+    | record k2 e m' =>
+      if k1 =? k2
+      then Some e
+      else find k1 m'
+    end.
+
+  (**
+     The [mem] predicate indicates wether there is an occurrence of the 
+     given key.
+   *)
+  Fixpoint mem {E:Type} (k1:K) (m:@mapping KDec E): bool :=
+    match m with
+    | empty => false
+    | record k2 e m' =>
+      if  k1 =? k2
+      then true
+      else mem k1 m'
+    end.
+  
+  (**
+     The [remove] function delete all the entries with the given key.
+   *)
+  Fixpoint remove {E:Type} (k1:K) (m:@mapping KDec E): mapping :=
+    match m with
+    | empty => empty
+    | record k2 e m' =>
+      if k1 =? k2
+      then remove k1 m'
+      else record k2 e (remove k1 m')
+    end.
+
+  (**
+     The [update] function return the [mapping] updated with the new value 
+     at the given key.
+     This function guaranties that there is never several occurences of the
+     same key in a [mapping].
+   *)
+  Definition update {E:Type} (k:K) (e:E) (m:mapping): mapping :=
+    if mem k m
+    then record k e (remove k m)
+    else record k e m.
+
+  (**
+     The [mapElements] function is map the given function on the elements
+     of the [mapping] without changing the keys.
+   *)
+  Fixpoint mapElements {E1 E2: Type}
+           (f: E1 -> E2)
+           (m: @mapping KDec E1): @mapping KDec E2 :=
+    match m with
+    | empty => empty
+    | record k e m' => record k (f e) (mapElements f m')
+    end.
 
   
 
